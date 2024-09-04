e912dc079c332726e73a848f25868804b4c97709
boburishmetov <boburmiro-77@mail.ru>
2024-09-04 13:32:53 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -8,3 +8,10 @@
 
 // const person: string = "Martin";
 // const count: number = 100; 
+
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
+
