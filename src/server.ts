84b4b77b04480cf1d578517f602b6f0847fa8568
boburishmetov <boburmiro-77@mail.ru>
2024-09-06 07:13:06 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,18 @@
 // const currentTime = moment().format("YYYY MM DD");
 // console.log(currentTime);
 
-
 // const person: string = "Martin";
-// const count: number = 100; 
+// const count: number = 100;
 
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("PORT:", process.env.PORT);
-console.log("MONGO_URL:", process.env.MONGO_URL);
+import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERROR on connection on MongoDB", err));
--- train.ts
+++ train.ts
@@ -1,4 +1,4 @@
-// G-TASK: 
+// G-TASK:
 
 /*
 
@@ -8,11 +8,11 @@ MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
 */
 
-function getHighestIndex(arr:number[]){
-    if(arr){
-        let maxvalue = Math.max(...arr);
-        return arr.indexOf(maxvalue);
-    }
+function getHighestIndex(arr: number[]) {
+  if (arr) {
+    let maxvalue = Math.max(...arr);
+    return arr.indexOf(maxvalue);
+  }
 }
 
 const array = [5, 21, 12, 21, 8];
