03e5f8f474db4f21a2f99a0e457a867fc99869e5
boburishmetov <boburmiro-77@mail.ru>
2024-09-28 20:16:40 +0900

    feat: develop createUsers business logic

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -39,4 +39,10 @@ routerAdmin.post(
 );
 
 /** User */
+routerAdmin.get(
+  "/user/all",
+  restaurantController.verifyRestaurant,
+  restaurantController.getUsers
+);
+
 export default routerAdmin;
