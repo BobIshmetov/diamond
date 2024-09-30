99ae1636397cf2ced393ba0af15c6496f462d565
boburishmetov <boburmiro-77@mail.ru>
2024-09-30 10:56:15 +0900

    feat: signip process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,25 @@
 console.log("Signup frontend javascript file");
+
+$(function () {});
+
+function validateSignupForm() {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".member-password").val();
+
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    alert("Pleace insert all requered inputs! ");
+    return false;
+  }
+
+  if (memberPassword !== confirmPassword) {
+    alert("Password differs, pleace check!");
+    return false;
+  }
+}
