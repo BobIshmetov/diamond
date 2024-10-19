4a70adab4c2bf17357b44195c0d5ffb90fb2d979
boburishmetov <boburmiro-77@mail.ru>
2024-10-19 20:23:18 +0900

    fix: train.ts W-TASK

--- src/train.ts
+++ src/train.ts
@@ -372,14 +372,33 @@ MASALAN: countChars("hello") return {h: 1, e: 1, l: 2, o: 1}
 */
 //JAVOB
 
-function countChars(str: string) {
-  let result: { [key: string]: number } = {};
+// function countChars(str: string) {
+//   let result: { [key: string]: number } = {};
 
-  for (let char of str) {
-    result[char] = (result[char] || 0) + 1;
+//   for (let char of str) {
+//     result[char] = (result[char] || 0) + 1;
+//   }
+
+//   return result;
+// }
+
+// console.log(countChars("hello"));
+
+/**************************************************************************************** */
+
+/*W-TASK:
+
+Shunday function yozing, uni array va number parametrlari bolsin. Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan array holatida qaytarsin
+MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]]*/
+
+//JAVOB
+
+function chunkArray(arr: any[], chunkSize: number): any[][] {
+  const result = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    result.push(arr.slice(i, i + chunkSize));
   }
-
   return result;
 }
 
-console.log(countChars("hello"));
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
